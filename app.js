1c78b72b666f71fab4515d690176123abcba500c
DC <dc@darrencattle.com>
2020-04-23 01:25:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,25 +18,39 @@ app.message('bing', async ({ message, say }) => {
   await say('BONG');
 });
 
+app.message('oo', async ({ message, say }) => {
+  await say('OOBOO');
+});
+
+app.message('chess', async ({ message, say }) => {
+  await say('chezz');
+});
+
+// MENTIONS
+app.event('app_mention', async ({ event, say , context }) => {
+  await say('IO 2 I AM OOBOO');
+});
+
+app.message(/echo (.*)/i, async ({ message, say, context }) => {
+  var user = message.user;
+  console.log(message.user);
+  //d = U0316HMAS
+  if(user == "U0316HMAS") {
+    await say(context.matches[1]);
+  }
+});
+
+/* FUNCTIONS
 app.message(/math (\S*) (\S*)/i, async ({ message, say, context }) => {
   const a = context.matches[1];
   const b = context.matches[2];
-  await say(a);
-});
+  var c = (+a) + (+b);
+  await say(c.toString());
+});*/
 
-app.message(/echo (.*)/i, ({ say, context }) => {
-  say(context.matches[1]);
-});
-
-// MENTION LISTENERS
-app.event('app_mention', async ({ event, say , context }) => {
-  await say('im not a ho');
-});
 
 
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3000);
-
   console.log('⚡️ Bolt app is running!');
 })();
